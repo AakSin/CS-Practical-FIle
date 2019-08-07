411e56a59ccf210a8cf0af339b33546e9cbc4204
Aakarsh Singh <aakarshsingh0909@gmail.com>
2019-08-07 23:07:10 +0530

    starter with ch-4

---
+++ Practical File/Ch-4/q10.py
@@ -0,0 +1,3 @@
+#Write a script to partition the string 'INSTITUTE" at the occurence of letter "T".
+a="INSTITUTE"
+print(a.split("T"))
---
+++ Practical File/Ch-4/q9.py
@@ -0,0 +1,3 @@
+a="My School"
+print(a.split("h"))
+#print(a.partition("h"))
