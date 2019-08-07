b68c02e91d1fd554cd49595094e71c1ca767b5dc
Aakarsh Singh <aakarshsingh0909@gmail.com>
2019-08-07 19:42:46 +0530

    organization update

--- String Workshhet/q4.py
+++
@@ -1,9 +0,0 @@
-a=input("enter string 1 ")
-b=input("enter string 2 ")
-
-c=a[0]+ b[1] + a[2:]
-d=b[0]+ a[1] + b[2:]
-
-e=c+" "+d
-
-print(e)
--- String Workshhet/q7.py
+++
@@ -1,9 +0,0 @@
-z=0
-while True:
-    a=input("enter a string")
-    if a[0]==a[-1] and len(a)>1:
-        z=z+1
-    if a=="0":
-        break
-
-print(z)
