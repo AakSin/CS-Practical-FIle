1f093ab7453944c41602dd3d2a9e65518f78cdf1
Aakarsh Singh <aakarshsingh0909@gmail.com>
2020-04-19 16:13:24 +0530

    q1 update

--- Practical File 20-21/Recursion/q1.py
+++ Practical File 20-21/Recursion/q1.py
@@ -1,11 +1,11 @@
 def LenoirSearch(Arr1D, l, r, x): 
     if r < l: 
         return -1
-    if arr[l] == x: 
+    if Arr1D[l] == x: 
         return l 
-    if arr[r] == x: 
+    if Arr1D[r] == x: 
         return r 
-    return LenoirSearch(arr, l+1, r-1, x) 
+    return LenoirSearch(Arr1D, l+1, r-1, x) 
   
 arr=[2,7,1,9]
-print(LenoirSearch(arr, 0, len(arr)-1, 7) )
+print(LenoirSearch(arr, 0, len(arr)-1, 1) )
